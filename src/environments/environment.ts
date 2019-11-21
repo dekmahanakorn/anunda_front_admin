4e077b3f51bc4a4680dddba9275e8ffddd5a2303
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2019-11-21 20:02:01 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDHL27PyZ6AtTT6L01JdjU_U6ju7DvT0rY",
+    authDomain: "anundatechfirstwebsite.firebaseapp.com",
+    databaseURL: "https://anundatechfirstwebsite.firebaseio.com",
+    projectId: "anundatechfirstwebsite",
+    storageBucket: "anundatechfirstwebsite.appspot.com",
+    messagingSenderId: "780011251240"
+  }
 };
 
 /*
