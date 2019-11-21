08ed3ad102ac66c74c80dac9d9f0eba6936f84fd
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2019-11-21 22:10:27 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {ToastrModule} from 'ngx-toastr';
+import { ToastrModule } from 'ngx-toastr';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -17,7 +17,7 @@ import { ComponentsModule } from './components/components.module';
 
 /* Firebase services */
 import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule} from '@angular/fire/firestore';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
 /* Firebase Auth service :  Run command for create file --> "ng g s shared/Authentication --spec=false" */
@@ -50,7 +50,7 @@ import { AboutComponent } from './pages/about/about.component';
     AboutComponent,
 
   ],
-  providers: [AuthenticationService,CreateService],
+  providers: [AuthenticationService, CreateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
