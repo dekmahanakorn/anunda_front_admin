7ff3b5bb0bcd992cafa88607f354bcfa04a80429
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2019-11-21 20:01:53 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ToastrModule} from 'ngx-toastr';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,6 +8,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { environment } from 'src/environments/environment';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -16,26 +18,20 @@ import { ComponentsModule } from './components/components.module';
 
 /* Firebase services */
 import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule} from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
 /* Firebase Auth service :  Run command for create file --> "ng g s shared/Authentication --spec=false" */
 import { AuthenticationService } from './shared/authentication.service';
+import { CreateService } from './shared/create.service';
+import { from } from 'rxjs';
 
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDHL27PyZ6AtTT6L01JdjU_U6ju7DvT0rY',
-  authDomain: 'anundatechfirstwebsite.firebaseapp.com',
-  databaseURL: 'https://anundatechfirstwebsite.firebaseio.com',
-  projectId: 'anundatechfirstwebsite',
-  storageBucket: 'anundatechfirstwebsite.appspot.com',
-  messagingSenderId: '780011251240',
-  appId: '1:780011251240:web:6eee98312158e9bfd4771d',
-};
 
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
+    ToastrModule.forRoot(),
     FormsModule,
     HttpClientModule,
     ComponentsModule,
@@ -44,7 +40,8 @@ const firebaseConfig = {
     AppRoutingModule,
 
     AngularFireAuthModule,
-    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   declarations: [
     AppComponent,
@@ -52,7 +49,7 @@ const firebaseConfig = {
     AuthLayoutComponent,
 
   ],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService,CreateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
