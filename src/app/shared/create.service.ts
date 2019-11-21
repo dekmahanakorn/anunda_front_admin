fb9602a61c5f4d43b1691f48b1dccd05a07d1806
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2019-11-21 20:02:04 +0700

    Create create.service.ts

---
+++ src/app/shared/create.service.ts
@@ -0,0 +1,16 @@
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Injectable } from '@angular/core';
+import { Create } from './create.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CreateService {
+  formData: Create;
+
+  constructor(private firestore: AngularFirestore) { }
+
+  getCreates() {
+    return this.firestore.collection('creates').snapshotChanges();
+  }
+}
