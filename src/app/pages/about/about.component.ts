694591b7024fa4a58b7b071bf36f1a7d5d04a545
Pakinackermen <actionsisty@gmail.com>
2019-11-25 21:33:29 +0700

    extact branch

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,6 +1,9 @@
+import { NgModule } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from 'src/app/shared/firebase.service';
 import { InterfaceAbout } from 'src/app/interface/interfaceAbout';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-about',
@@ -8,23 +11,29 @@ import { InterfaceAbout } from 'src/app/interface/interfaceAbout';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  aboutPage: FormGroup;
+  constructor(private firebaseService: FirebaseService, private formBuilder: FormBuilder) { }
 
-  constructor( private firebaseService: FirebaseService) { }
-
-
+  // aboutPage = new FormGroup({
+  //   title: new FormControl(''),
+  //   description: new FormControl(''),
+  // });
   // private data: InterfaceAbout;
 
   ngOnInit() {
-
+      this.aboutPage = this.formBuilder.group({
+        title: new FormControl(),
+        description: new FormControl()
+      });
   }
-  
+
   test(data: InterfaceAbout): void {
     data = {};
     data.descripttion = 'To connect firebase';
     data.name = 'I,m sirmerfang';
 
     console.log('AboutComponent', data);
- 
+
     this.firebaseService.createDB(data);
   }
 }
