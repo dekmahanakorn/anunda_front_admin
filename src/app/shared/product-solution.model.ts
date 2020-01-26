b9324a30bebdcb047d7d81b79a9a8fbf0ec8228b
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-01-26 16:28:56 +0700

    Update product-solution.model.ts

--- src/app/shared/product-solution.model.ts
+++ src/app/shared/product-solution.model.ts
@@ -2,8 +2,10 @@ export class ProductSolution {
     id: string;
     category_id: string;
     name: string;
-    image_url: string;
     price: string;
     size: string;
+    image_url: string;
+    path_img: string;
     description: string;
+    timestamp: string;
 }
